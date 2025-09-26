485be4481a201a8597ecd7066dd9d4797153e3c9
pavani <bathinapavani28@gmail.com>
2025-09-26 00:49:50 -0500

    second

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (replace with valid AMI in your region)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "MyAppServer"
+  }
+}
